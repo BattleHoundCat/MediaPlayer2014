6a843cdd1b2a063e234705b8522794ce3c12c217
IgorOsavoluk <36710349+BattleHoundCat@users.noreply.github.com>
2021-03-08 12:40:56 +0400

    Add files via upload

---
+++ MyMediaPlayer/CheckForm.Designer.vb
@@ -0,0 +1,163 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class CheckForm
+    Inherits System.Windows.Forms.Form
+
+    'Форма переопределяет dispose для очистки списка компонентов.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Является обязательной для конструктора форм Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'Примечание: следующая процедура является обязательной для конструктора форм Windows Forms
+    'Для ее изменения используйте конструктор форм Windows Form.  
+    'Не изменяйте ее в редакторе исходного кода.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Yepbtn = New System.Windows.Forms.Button()
+        Me.Fonbtn = New System.Windows.Forms.Button()
+        Me.Delbtn = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.ConfirmBtn = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.ClearBtn = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Yepbtn
+        '
+        Me.Yepbtn.BackColor = System.Drawing.Color.Yellow
+        Me.Yepbtn.Location = New System.Drawing.Point(136, 123)
+        Me.Yepbtn.Name = "Yepbtn"
+        Me.Yepbtn.Size = New System.Drawing.Size(75, 23)
+        Me.Yepbtn.TabIndex = 0
+        Me.Yepbtn.Text = "Классно!"
+        Me.Yepbtn.UseVisualStyleBackColor = False
+        '
+        'Fonbtn
+        '
+        Me.Fonbtn.BackColor = System.Drawing.Color.FromArgb(CType(CType(128, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.Fonbtn.Location = New System.Drawing.Point(217, 123)
+        Me.Fonbtn.Name = "Fonbtn"
+        Me.Fonbtn.Size = New System.Drawing.Size(75, 23)
+        Me.Fonbtn.TabIndex = 1
+        Me.Fonbtn.Text = "Фон"
+        Me.Fonbtn.UseVisualStyleBackColor = False
+        '
+        'Delbtn
+        '
+        Me.Delbtn.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.Delbtn.Location = New System.Drawing.Point(180, 94)
+        Me.Delbtn.Name = "Delbtn"
+        Me.Delbtn.Size = New System.Drawing.Size(75, 23)
+        Me.Delbtn.TabIndex = 2
+        Me.Delbtn.Text = "Удалить"
+        Me.Delbtn.UseVisualStyleBackColor = False
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(12, 5)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(413, 20)
+        Me.TextBox1.TabIndex = 3
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(12, 38)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(413, 20)
+        Me.TextBox2.TabIndex = 3
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Location = New System.Drawing.Point(127, 165)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(191, 21)
+        Me.ComboBox1.TabIndex = 4
+        '
+        'ConfirmBtn
+        '
+        Me.ConfirmBtn.BackColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.ConfirmBtn.Location = New System.Drawing.Point(324, 165)
+        Me.ConfirmBtn.Name = "ConfirmBtn"
+        Me.ConfirmBtn.Size = New System.Drawing.Size(92, 33)
+        Me.ConfirmBtn.TabIndex = 5
+        Me.ConfirmBtn.Text = "Отправить"
+        Me.ConfirmBtn.UseVisualStyleBackColor = False
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(253, 149)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(13, 13)
+        Me.Label1.TabIndex = 6
+        Me.Label1.Text = "0"
+        '
+        'ClearBtn
+        '
+        Me.ClearBtn.BackColor = System.Drawing.Color.FromArgb(CType(CType(224, Byte), Integer), CType(CType(224, Byte), Integer), CType(CType(224, Byte), Integer))
+        Me.ClearBtn.Location = New System.Drawing.Point(127, 192)
+        Me.ClearBtn.Name = "ClearBtn"
+        Me.ClearBtn.Size = New System.Drawing.Size(85, 23)
+        Me.ClearBtn.TabIndex = 7
+        Me.ClearBtn.Text = "Clear"
+        Me.ClearBtn.UseVisualStyleBackColor = False
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(142, 149)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(70, 13)
+        Me.Label2.TabIndex = 8
+        Me.Label2.Text = "Всего папок"
+        '
+        'CheckForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.ClientSize = New System.Drawing.Size(433, 389)
+        Me.ControlBox = False
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.ClearBtn)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.ConfirmBtn)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Delbtn)
+        Me.Controls.Add(Me.Fonbtn)
+        Me.Controls.Add(Me.Yepbtn)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "CheckForm"
+        Me.Text = "CheckForm"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Yepbtn As System.Windows.Forms.Button
+    Friend WithEvents Fonbtn As System.Windows.Forms.Button
+    Friend WithEvents Delbtn As System.Windows.Forms.Button
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
+    Friend WithEvents ComboBox1 As System.Windows.Forms.ComboBox
+    Friend WithEvents ConfirmBtn As System.Windows.Forms.Button
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents ClearBtn As System.Windows.Forms.Button
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+End Class
---
+++ MyMediaPlayer/CheckForm.vb
@@ -0,0 +1,195 @@
+﻿Imports System.IO
+Imports Excel = Microsoft.Office.Interop.Excel
+Public Class CheckForm
+    Dim xlApp As Excel.Application = New Excel.Application
+    Dim xlWorkBook As Excel.Workbook = xlApp.Workbooks.Open("f:\Хаос\MusicList.xls")
+    Dim xlWorkSheet As Excel.Worksheet = xlWorkBook.Worksheets("Лист1")
+    Dim musicname As String
+    Dim sourcemusic As String
+    Dim Category As String
+    Private Sub Yepbtn_Click(sender As Object, e As EventArgs) Handles Yepbtn.Click
+            If CheckDir(TextBox1.Text) Then
+                Category = "Классно"
+                sourcemusic = Form1.wmp.currentMedia.sourceURL
+                musicname = IO.Path.GetFileName(Form1.wmp.currentMedia.sourceURL)
+                'если есть директории в папке Классно
+                Dim pathes() As String
+                pathes = Directory.GetDirectories(TextBox1.Text)
+            For i = 0 To pathes.Length - 1
+                Dim DI As New IO.DirectoryInfo(pathes(i))
+                ComboBox1.Items.Add(DI.Name)
+            Next
+            Label1.Text = pathes.Length
+        Else
+            'если их нет
+            Category = "Классно"
+            sourcemusic = Form1.wmp.currentMedia.sourceURL
+            musicname = IO.Path.GetFileName(Form1.wmp.currentMedia.sourceURL)
+        End If
+        Fonbtn.Enabled = False
+        Yepbtn.Enabled = False
+        ConfirmBtn.Enabled = True
+        ComboBox1.Enabled = True
+    End Sub
+
+    Private Sub Fonbtn_Click(sender As Object, e As EventArgs) Handles Fonbtn.Click
+            If CheckDir(TextBox2.Text) Then
+                Category = "Фон"
+                sourcemusic = Form1.wmp.currentMedia.sourceURL
+                musicname = IO.Path.GetFileName(Form1.wmp.currentMedia.sourceURL)
+                'если есть директории в папке Классно
+                Dim pathes() As String
+                pathes = Directory.GetDirectories(TextBox2.Text)
+                For i = 0 To pathes.Length - 1
+                    Dim DI As New IO.DirectoryInfo(pathes(i))
+                    ComboBox1.Items.Add(DI.Name)
+            Next
+            Label1.Text = pathes.Length
+            Else
+                'если их нет
+                Category = "Фон"
+                sourcemusic = Form1.wmp.currentMedia.sourceURL
+                musicname = IO.Path.GetFileName(Form1.wmp.currentMedia.sourceURL)
+        End If
+        Fonbtn.Enabled = False
+        Yepbtn.Enabled = False
+        ConfirmBtn.Enabled = True
+        ComboBox1.Enabled = True
+    End Sub
+
+    Private Sub Delbtn_Click(sender As Object, e As EventArgs) Handles Delbtn.Click
+        File.Delete(Form1.wmp.currentMedia.sourceURL)
+        MsgBox("Эта хреновая музыка удалена !" & vbCrLf & Form1.wmp.currentMedia.sourceURL)
+        Me.Close()
+    End Sub
+
+    Private Sub CheckForm_Load(sender As Object, e As EventArgs) Handles Me.Load
+        TextBox1.Text = "D:\Новая жизнь\Новое\Хобби\Развлечения\Музыка\Автоматом\Классно\"
+        TextBox2.Text = "D:\Новая жизнь\Новое\Хобби\Развлечения\Музыка\Автоматом\Фон\"
+        ConfirmBtn.Enabled = False
+    End Sub
+    Private Sub MoveFile(ByVal sourceFile As String, ByVal FileName As String, ByVal path As String, ByVal Category As String, ByVal localpath As String)
+        If File.Exists(sourceFile) Then
+            'если исходный файл существует,то перемещаем его в нужную папку
+            If Directory.Exists(path & localpath) Then
+                'если есть директория,то проверяем файл
+                If File.Exists(path & "\" & localpath & "\" & FileName) Then
+                    'здесь типа проверка,существует ли подобный файл с подобным названием в уже целевой папке
+                    MsgBox("Файл с таким названием уже существует,он будет переименован")
+                    Dim i As Integer = Form1.Random(1, 100000)
+
+                    WriteExcel(i & FileName, localpath, Category, path & localpath)
+                    File.Move(sourceFile, path & "\" & localpath & "\" & i & FileName)
+                Else
+                    WriteExcel(FileName, localpath, Category, path & localpath)
+                    File.Move(sourceFile, path & "\" & localpath & "\" & FileName)
+                End If
+            Else
+                'если ее нет,то создаем
+                Directory.CreateDirectory(path & localpath)
+                WriteExcel(FileName, localpath, Category, path & localpath)
+                File.Move(sourceFile, path & localpath & "\" & FileName)
+            End If
+
+        End If
+    End Sub
+    Public Sub WriteExcel(ByVal filename As String, ByVal artist As String, ByVal Category As String, ByVal finishPath As String)
+        Dim cell_text As Integer
+        Dim xlCells As Excel.Range = Nothing
+        Dim range As Excel.Range = Nothing
+        xlCells = xlWorkSheet.Cells(1, 1)
+        cell_text = Val(xlCells.Value)
+        Dim i As Integer = 0
+        If cell_text > 0 Then
+            'записываем в след.колонку
+            xlWorkSheet.Cells(cell_text + 3, 1).Value = cell_text + 1
+            xlWorkSheet.Cells(cell_text + 3, 2).Value = filename
+            xlWorkSheet.Cells(cell_text + 3, 3).Value = artist
+            xlWorkSheet.Cells(cell_text + 3, 4).Value = Category
+            xlWorkSheet.Cells(cell_text + 3, 5).Value = finishPath
+            xlWorkBook.Save()
+        Else
+            'иначе записываем в первую колонку
+            xlWorkSheet.Cells(i + 3, 1).Value = 1
+            xlWorkSheet.Cells(i + 3, 2).Value = filename
+            xlWorkSheet.Cells(i + 3, 3).Value = artist
+            xlWorkSheet.Cells(i + 3, 4).Value = Category
+            xlWorkSheet.Cells(i + 3, 5).Value = finishPath
+            xlWorkBook.Save()
+        End If
+    End Sub
+    Private Sub Form1_Closing(sender As Object, e As System.ComponentModel.CancelEventArgs) Handles MyBase.Closing
+        xlWorkBook.Close()
+        xlApp.Quit()
+        'xlApp.Dispose()
+        releaseObject(xlApp)
+        releaseObject(xlWorkBook)
+        releaseObject(xlWorkSheet)
+    End Sub
+    Private Sub releaseObject(ByVal obj As Object)
+        Try
+            System.Runtime.InteropServices.Marshal.ReleaseComObject(obj)
+            obj = Nothing
+        Catch ex As Exception
+            obj = Nothing
+        Finally
+            GC.Collect()
+        End Try
+    End Sub
+    Private Function CheckDir(ByVal targetDir As String) As Boolean
+        If Directory.Exists(targetDir) Then
+            Dim str() As String
+            str = Directory.GetDirectories(targetDir)
+            If str.Length > 0 Then
+                Return True
+            Else
+                Return False
+            End If
+        Else
+            Directory.CreateDirectory(targetDir)
+        End If
+    End Function
+
+    Private Sub ConfirmBtn_Click(sender As Object, e As EventArgs) Handles ConfirmBtn.Click
+        If Category <> "" Then
+            If Category = "Классно" Then
+                If Not IsNothing(ComboBox1.SelectedItem) Then
+                    MoveFile(sourcemusic, musicname, TextBox1.Text, Category, ComboBox1.SelectedItem.ToString)
+                    Me.Close()
+                Else
+                    If ComboBox1.Text <> "" Then
+                        MoveFile(sourcemusic, musicname, TextBox1.Text, Category, ComboBox1.Text)
+                        Me.Close()
+                    Else
+                        MsgBox("Выберите папку или напишите ее в названии")
+                    End If
+                End If
+            Else
+                If Not IsNothing(ComboBox1.SelectedItem) Then
+                    MoveFile(sourcemusic, musicname, TextBox2.Text, Category, ComboBox1.SelectedItem.ToString)
+                    Me.Close()
+                Else
+                    If ComboBox1.Text <> "" Then
+                        MoveFile(sourcemusic, musicname, TextBox2.Text, Category, ComboBox1.Text)
+                        Me.Close()
+                    Else
+                        MsgBox("Выберите папку или напишите ее в названии")
+                    End If
+                End If
+            End If
+            
+        Else
+            MsgBox("Выберите категорию музыки:Классно,Фон,или Удалить")
+        End If
+    End Sub
+
+    Private Sub ClearBtn_Click(sender As Object, e As EventArgs) Handles ClearBtn.Click
+        ComboBox1.Items.Clear()
+        ComboBox1.Text = ""
+        ConfirmBtn.Enabled = False
+        ComboBox1.Enabled = False
+        Fonbtn.Enabled = True
+        Yepbtn.Enabled = True
+        Label1.Text = 0
+    End Sub
+End Class
